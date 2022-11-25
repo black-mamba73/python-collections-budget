995b7afc325203d3b16b39f2152e8a891b56ebe8
black-mamba73 <j.n.orihito.111@gmail.com>
2022-11-25 21:11:41 +1000

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,2 +1,7 @@
 class BudgetList():
-    pass
+    def __init__(self, budget):
+    self.budget = budget
+    self.sum_expenses = 0
+    self.expenses = []
+    self.sum_overagets = 0
+    self.overages = []
